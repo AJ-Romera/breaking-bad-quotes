e73e8d8e9d43552d49fcbe3b6154c999431f6273
AJ-Romera <ajromeraalfonso@gmail.com>
2021-03-04 19:16:56 +0100

    CHNG: Style Quote Component w/ Styled Components && Use a Media Query

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const Button = styled.button`
     background-size: 400px;
     font-family: Arial, Helvetica, sans-serif;
     color: #fff;
-    margin-top: 3rem;
+    margin-top: 1rem;
     padding: 1rem 3rem;
     font-size: 2rem;
     border: 2px solid black;
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const QuoteContainer = styled.div`
+    padding: 1rem 2rem;
+    border-radius: 0.5rem;
+    background-color: #fff;
+    max-width: 1200px;
+
+    @media (min-width: 992px) {
+        margin-top: 15rem;
+    }
+
+    h1 {
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+        position: relative;
+        padding-left: 4rem;
+        font-size: 1.7rem;
+
+        &::before {
+            content: open-quote;
+            font-size: 10rem;
+            color: black;
+            position: absolute;
+            left: -1rem;
+            top: -2rem;
+        }
+    }
+    p {
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: 1.2rem;
+        font-weight: bold;
+        text-align: right;
+        color: #666;
+        margin-top: 2rem;
+    }
+`;
 
 function Frase({ quote }) {
+    /* if (Object.keys(quote).length === 0) {
+        return null;
+    } */
+
     return (
-        <div>
+        <QuoteContainer>
             <h1>{quote.quote}</h1>
             <p>- {quote.author}</p>
-        </div>
+        </QuoteContainer>
     );
 }
 
